7e463d5859fe0c5e8ccce4bcc562876f89cb6cd5
somethingsaid <kwong.billy@gmail.com>
2016-03-01 14:40:01 -0500

    Skeletoned some functions

--- js/app.js
+++ js/app.js
@@ -12,6 +12,38 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- Getting user's guess when they click Guess button ---*/
+
 });
+/*--- Initialize global variables ---*/
+/*--- this block onwards might have to move to the top of file ---*/
+var secretNum = 0;
+var guessCount = 0;
+var userGuess = 0;
+var feedback = '';
 
+/*--- Functions ---*/
+// Generate random number between 1 and 100 (inclusive)
+function setSecretNum() {
+	secretNum = Math.floor(Math.random() * (100)) + 1;
+}
 
+// Starting a new game
+function newGame() {
+	// reset global variables
+	getNum();
+	guessCount = 0;
+	feedback = '';
+	//details
+}
+
+// Get user input and perform hygiene check
+function getGuess() {
+
+}
+
+// Generating feedback (Hot or Cold)
+function feedback(answer, guess) {
+    //if () {
+    //}
+}
